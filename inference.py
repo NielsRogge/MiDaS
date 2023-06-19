c746efcadef3a2e318797867c43b9052d830ea12
Niels <niels.rogge1@gmail.com>
2023-06-19 16:43:38 +0200

    Simplify script

--- inference.py
+++ inference.py
@@ -43,20 +43,20 @@ def process(device, model, model_type, image, input_size, target_size, optimize,
         prediction = cv2.resize(prediction, dsize=target_size,
                                 interpolation=cv2.INTER_CUBIC)
     else:
-        sample = torch.from_numpy(image).to(device).unsqueeze(0)
+        # sample = torch.from_numpy(image).to(device).unsqueeze(0)
 
-        if optimize and device == torch.device("cuda"):
-            if first_execution:
-                print("  Optimization to half-floats activated. Use with caution, because models like Swin require\n"
-                      "  float precision to work properly and may yield non-finite depth values to some extent for\n"
-                      "  half-floats.")
-            sample = sample.to(memory_format=torch.channels_last)
-            sample = sample.half()
+        # if optimize and device == torch.device("cuda"):
+        #     if first_execution:
+        #         print("  Optimization to half-floats activated. Use with caution, because models like Swin require\n"
+        #               "  float precision to work properly and may yield non-finite depth values to some extent for\n"
+        #               "  half-floats.")
+        #     sample = sample.to(memory_format=torch.channels_last)
+        #     sample = sample.half()
 
-        if first_execution or not use_camera:
-            height, width = sample.shape[2:]
-            print(f"    Input resized to {width}x{height} before entering the encoder")
-            first_execution = False
+        # if first_execution or not use_camera:
+        #     height, width = sample.shape[2:]
+        #     print(f"    Input resized to {width}x{height} before entering the encoder")
+        #     first_execution = False
 
         ## start of insert cats image
         import requests
@@ -77,7 +77,7 @@ def process(device, model, model_type, image, input_size, target_size, optimize,
         prediction = (
             torch.nn.functional.interpolate(
                 prediction.unsqueeze(1),
-                size=target_size[::-1],
+                size=image.size[::-1],
                 mode="bicubic",
                 align_corners=False,
             )
@@ -87,34 +87,6 @@ def process(device, model, model_type, image, input_size, target_size, optimize,
         )
 
     return prediction
-
-
-def create_side_by_side(image, depth, grayscale):
-    """
-    Take an RGB image and depth map and place them side by side. This includes a proper normalization of the depth map
-    for better visibility.
-
-    Args:
-        image: the RGB image
-        depth: the depth map
-        grayscale: use a grayscale colormap?
-
-    Returns:
-        the image and depth map place side by side
-    """
-    depth_min = depth.min()
-    depth_max = depth.max()
-    normalized_depth = 255 * (depth - depth_min) / (depth_max - depth_min)
-    normalized_depth *= 3
-
-    right_side = np.repeat(np.expand_dims(normalized_depth, 2), 3, axis=2) / 3
-    if not grayscale:
-        right_side = cv2.applyColorMap(np.uint8(right_side), cv2.COLORMAP_INFERNO)
-
-    if image is None:
-        return right_side
-    else:
-        return np.concatenate((image, right_side), axis=1)
 
 
 def run(input_path, output_path, model_path, model_type="dpt_beit_large_512", optimize=False, side=False, height=None,
@@ -152,70 +124,26 @@ def run(input_path, output_path, model_path, model_type="dpt_beit_large_512", op
         os.makedirs(output_path, exist_ok=True)
 
     print("Start processing")
+    image = None
+    target_size = [512,512]
 
-    if input_path is not None:
-        if output_path is None:
-            print("Warning: No output path specified. Images will be processed but not shown or stored anywhere.")
-        for index, image_name in enumerate(image_names):
+    # compute
+    with torch.no_grad():
+        prediction = process(device, model, model_type, image, (net_w, net_h), target_size,
+                                optimize, False)
 
-            print("  Processing {} ({}/{})".format(image_name, index + 1, num_images))
-
-            # input
-            original_image_rgb = utils.read_image(image_name)  # in [0, 1]
-            image = transform({"image": original_image_rgb})["image"]
-
-            # compute
-            with torch.no_grad():
-                prediction = process(device, model, model_type, image, (net_w, net_h), original_image_rgb.shape[1::-1],
-                                     optimize, False)
-
-            # output
-            if output_path is not None:
-                filename = os.path.join(
-                    output_path, os.path.splitext(os.path.basename(image_name))[0] + '-' + model_type
-                )
-                if not side:
-                    utils.write_depth(filename, prediction, grayscale, bits=2)
-                else:
-                    original_image_bgr = np.flip(original_image_rgb, 2)
-                    content = create_side_by_side(original_image_bgr*255, prediction, grayscale)
-                    cv2.imwrite(filename + ".png", content)
-                utils.write_pfm(filename + ".pfm", prediction.astype(np.float32))
-
-    else:
-        with torch.no_grad():
-            fps = 1
-            video = VideoStream(0).start()
-            time_start = time.time()
-            frame_index = 0
-            while True:
-                frame = video.read()
-                if frame is not None:
-                    original_image_rgb = np.flip(frame, 2)  # in [0, 255] (flip required to get RGB)
-                    image = transform({"image": original_image_rgb/255})["image"]
-
-                    prediction = process(device, model, model_type, image, (net_w, net_h),
-                                         original_image_rgb.shape[1::-1], optimize, True)
-
-                    original_image_bgr = np.flip(original_image_rgb, 2) if side else None
-                    content = create_side_by_side(original_image_bgr, prediction, grayscale)
-                    cv2.imshow('MiDaS Depth Estimation - Press Escape to close window ', content/255)
-
-                    if output_path is not None:
-                        filename = os.path.join(output_path, 'Camera' + '-' + model_type + '_' + str(frame_index))
-                        cv2.imwrite(filename + ".png", content)
-
-                    alpha = 0.1
-                    if time.time()-time_start > 0:
-                        fps = (1 - alpha) * fps + alpha * 1 / (time.time()-time_start)  # exponential moving average
-                        time_start = time.time()
-                    print(f"\rFPS: {round(fps,2)}", end="")
-
-                    if cv2.waitKey(1) == 27:  # Escape key
-                        break
-
-                    frame_index += 1
-        print()
+    # # output
+    # if output_path is not None:
+    #     filename = os.path.join(
+    #         output_path, os.path.splitext(os.path.basename(image_name))[0] + '-' + model_type
+    #     )
+    #     if not side:
+    #         utils.write_depth(filename, prediction, grayscale, bits=2)
+    #     else:
+    #         original_image_bgr = np.flip(original_image_rgb, 2)
+    #         content = create_side_by_side(original_image_bgr*255, prediction, grayscale)
+    #         cv2.imwrite(filename + ".png", content)
+    #     utils.write_pfm(filename + ".pfm", prediction.astype(np.float32))
 
     print("Finished")
 
